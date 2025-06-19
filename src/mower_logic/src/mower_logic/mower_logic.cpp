b2ba0eba14b57f135126e49fa771db0352f38441
Yann Lossouarn <yann@lossouarn.net>
2025-06-20 00:33:02 +0200

    replaced mower_enabled() call by shall_mow()

--- src/mower_logic/src/mower_logic/mower_logic.cpp
+++ src/mower_logic/src/mower_logic/mower_logic.cpp
@@ -515,8 +515,8 @@ void checkSafety(const ros::TimerEvent &timer_event) {
   // ROS_INFO_STREAM_THROTTLE(3, "YL: before setMowerEnabled, currentBehavior: " << (currentBehavior) << "");
   // ROS_INFO_STREAM_THROTTLE(3, "YL: before setMowerEnabled, currentBehavior != nullptr: " << (currentBehavior != nullptr) << "");
   ROS_INFO_STREAM_THROTTLE(3, "YL: before setMowerEnabled, mowerAllowed: " << (mowerAllowed) << "");
-  ROS_INFO_STREAM( "YL: before setMowerEnabled, currentBehavior->mower_enabled(): " << (currentBehavior->mower_enabled()) << "");
-  // ROS_INFO_STREAM_THROTTLE(1, "YL: before setMowerEnabled, consigne: " << (currentBehavior != nullptr && mowerAllowed && currentBehavior->mower_enabled()) << "");
+  ROS_INFO_STREAM( "YL: before setMowerEnabled, currentBehavior->shall_mow(): " << (currentBehavior->shall_mow()) << "");
+  // ROS_INFO_STREAM_THROTTLE(1, "YL: before setMowerEnabled, consigne: " << (currentBehavior != nullptr && mowerAllowed && currentBehavior->shall_mow()) << "");
   // enable the mower (if not aleady) if mowerAllowed is still true after checks and bahavior agrees
   setMowerEnabled(currentBehavior != nullptr && mowerAllowed && currentBehavior->shall_mow());
 
