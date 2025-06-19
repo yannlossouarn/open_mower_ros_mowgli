6ef3a95b61333126a414421565cc1249aa553ea8
Yann Lossouarn <yann@lossouarn.net>
2025-06-19 21:50:31 +0200

    Ajout d'une condition sur l'état courant pour activer le rotor

--- src/mower_logic/src/mower_logic/mower_logic.cpp
+++ src/mower_logic/src/mower_logic/mower_logic.cpp
@@ -503,15 +503,15 @@ void checkSafety(const ros::TimerEvent &timer_event) {
     }
   }
 
-  ROS_INFO_STREAM_THROTTLE(3, "YL: before setMowerEnabled, currentBehavior: " << (currentBehavior) << "");
-  ROS_INFO_STREAM_THROTTLE(3, "YL: before setMowerEnabled, currentBehavior != nullptr: " << (currentBehavior != nullptr) << "");
+  // ROS_INFO_STREAM_THROTTLE(3, "YL: before setMowerEnabled, currentBehavior: " << (currentBehavior) << "");
+  // ROS_INFO_STREAM_THROTTLE(3, "YL: before setMowerEnabled, currentBehavior != nullptr: " << (currentBehavior != nullptr) << "");
   ROS_INFO_STREAM_THROTTLE(3, "YL: before setMowerEnabled, mowerAllowed: " << (mowerAllowed) << "");
-  ROS_INFO_STREAM_THROTTLE(3, "YL: before setMowerEnabled, currentBehavior->mower_enabled(): " << (currentBehavior->mower_enabled()) << "");
-  ROS_INFO_STREAM_THROTTLE(3, "YL: before setMowerEnabled, consigne: " << (currentBehavior != nullptr && mowerAllowed && currentBehavior->mower_enabled()) << "");
+  ROS_INFO_STREAM( "YL: before setMowerEnabled, currentBehavior->mower_enabled(): " << (currentBehavior->mower_enabled()) << "");
+  // ROS_INFO_STREAM_THROTTLE(1, "YL: before setMowerEnabled, consigne: " << (currentBehavior != nullptr && mowerAllowed && currentBehavior->mower_enabled()) << "");
   // enable the mower (if not aleady) if mowerAllowed is still true after checks and bahavior agrees
-  setMowerEnabled(currentBehavior != nullptr && mowerAllowed && !currentBehavior->mower_enabled());
+  setMowerEnabled(currentBehavior != nullptr && mowerAllowed && "AREA_RECORDING" == currentBehavior->state_name() && !currentBehavior->mower_enabled());
 
-  ROS_INFO_STREAM_THROTTLE(3, "YL: after  setMowerEnabled, currentBehavior: " << currentBehavior << ", mowerAllowed: " << mowerAllowed << "");
+  // ROS_INFO_STREAM_THROTTLE(3, "YL: after  setMowerEnabled, currentBehavior: " << currentBehavior << ", mowerAllowed: " << mowerAllowed << "");
 
   double battery_percent = (last_status.v_battery - last_config.battery_empty_voltage) /
                            (last_config.battery_full_voltage - last_config.battery_empty_voltage);
