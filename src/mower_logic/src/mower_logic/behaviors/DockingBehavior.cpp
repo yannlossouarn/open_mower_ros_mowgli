7eb2a3280ff2c930bc2f8d9b9ce30a9f6d765dfb
Yann Lossouarn <yann@lossouarn.net>
2026-04-09 11:09:23 +0200

    Additional trace log

--- src/mower_logic/src/mower_logic/behaviors/DockingBehavior.cpp
+++ src/mower_logic/src/mower_logic/behaviors/DockingBehavior.cpp
@@ -44,6 +44,7 @@ DockingBehavior::DockingBehavior() {
 
 bool DockingBehavior::approach_docking_point() {
   ROS_INFO_STREAM("Calculating approach path");
+  ROS_INFO_STREAM("Additional traces");
 
   // Calculate a docking approaching point behind the actual docking point
   tf2::Quaternion quat;
